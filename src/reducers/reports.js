227fce63346330f2077ea1805e06a0afa9654158
mansur2021 <102415890+mansur2021@users.noreply.github.com>
2022-05-20 14:18:28 +0300

    first commit

---
+++ src/reducers/reports.js
@@ -0,0 +1,151 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+export const getReports = createAsyncThunk(
+  "Reports/getReports",
+  async (_, { rejectWithValue, dispatch }) => {
+    try {
+      const { data } = await axios.get(
+        "https://6273c5f6345e1821b2213ae6.mockapi.io/repost"
+      );
+      console.log(data);
+      return data;
+    } catch (error) {
+      return rejectWithValue(error.message);
+    }
+  }
+);
+
+export const addReports = createAsyncThunk(
+  "Reports/addReports",
+  async (_, { rejectWithValue, dispatch, getState }) => {
+    const report = getState().reports.report;
+    try {
+      const { data } = await axios.post(
+        "https://6273c5f6345e1821b2213ae6.mockapi.io/repost",
+        report
+      );
+
+      dispatch(getReports());
+      return data;
+    } catch (error) {
+      return rejectWithValue(error.message);
+    }
+  }
+);
+
+export const editReports = createAsyncThunk(
+  "reports/editReports",
+  async (_, { rejectWithValue, dispatch, getState }) => {
+    const report = getState().reports.report;
+    const id = getState().reports.id;
+    try {
+      const { data } = await axios.put(
+        `https://6273c5f6345e1821b2213ae6.mockapi.io/repost/${id}`,
+        report
+      );
+
+      dispatch(getReports());
+      return data;
+    } catch (error) {
+      return rejectWithValue(error.message);
+    }
+  }
+);
+
+export const deleteReports = createAsyncThunk(
+  "reports/deleteReports",
+  async (_, { rejectWithValue, dispatch, getState }) => {
+    const id = getState().reports.id;
+
+    try {
+      const { data } = await axios.delete(
+        `https://6273c5f6345e1821b2213ae6.mockapi.io/repost/${id}`
+      );
+
+      dispatch(getReports());
+      return data;
+    } catch (error) {
+      return rejectWithValue(error.message);
+    }
+  }
+);
+
+const setLoading = (state) => {
+  state.loading = true;
+};
+
+const setError = (state, action) => {
+  state.loading = false;
+  state.status = action.payload.error;
+};
+
+const initialReport = {
+  name: "",
+  model: "",
+  motor: null,
+};
+export const slice = createSlice({
+  name: "reports",
+  initialState: {
+    reports: [],
+    report: initialReport,
+    id: null,
+    addModal: false,
+    editModal: false,
+    deleteModal: false,
+    loading: false,
+    status: null,
+  },
+  reducers: {
+    handleChange: (state, action) => {
+      const { name, value } = action.payload;
+
+      state.report[name] = value;
+    },
+    handleModalOpenAndClose: (state, action) => {
+      const { name, value, id = null } = action.payload;
+      state[name] = value;
+      state.report = initialReport;
+      if (id) {
+        state.id = id;
+        state.report = state.reports.find((elem) => elem.id === id);
+      }
+    },
+  },
+  extraReducers: {
+    [getReports.pending]: setLoading,
+    [getReports.fulfilled]: (state, action) => {
+      state.loading = false;
+      state.reports = action.payload;
+    },
+    [getReports.rejected]: (state) => {
+      state.reports = [];
+    },
+    [addReports.pending]: setLoading,
+    [addReports.fulfilled]: (state, action) => {
+      state.loading = false;
+      state.status = action.payload;
+      state.addModal = false;
+    },
+    [addReports.rejected]: setError,
+    [editReports.pending]: setLoading,
+    [editReports.fulfilled]: (state, action) => {
+      state.loading = false;
+      state.status = action.payload;
+      state.editModal = false;
+    },
+    [editReports.rejected]: setError,
+    [deleteReports.pending]: setLoading,
+    [deleteReports.fulfilled]: (state, action) => {
+      state.loading = false;
+      state.status = action.payload;
+      state.deleteModal = false;
+    },
+    [deleteReports.rejected]: setError,
+  },
+});
+
+export const { handleChange, handleModalOpenAndClose } = slice.actions;
+
+export default slice.reducer;
